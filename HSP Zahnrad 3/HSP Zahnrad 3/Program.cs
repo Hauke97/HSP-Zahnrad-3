a5d81a1bb7bb3ca2f74fc368c3aa85d577587a3e
a-rondorf <72039786+a-rondorf@users.noreply.github.com>
2020-10-27 16:43:20 +0100

    Update Program.cs

--- HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
+++ HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
@@ -6,6 +6,7 @@ namespace HSP_Zahnrad_3
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Console.WriteLine("Geben Sie den Modul ein");
             double m = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Geben Sie die Zähnezahl ein");
@@ -14,6 +15,30 @@ namespace HSP_Zahnrad_3
             double d = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Geben Sie die Breite ein");
             double b = Convert.ToDouble(Console.ReadLine());
+=======
+            
+
+
+            Console.WriteLine("Geben Sie den Modul ein");
+            double m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben Sie den Teilkreisdurchmesser ein");
+            double d = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Geben Sie das Kopfspiel ein");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double df = d - 2 * (m + c);
+            Console.WriteLine("Ergebnis für df: " + df + "mm");
+
+            Console.WriteLine("Gebenn Sie die Zähnezahl ein");
+            double z = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte geben Sie den Winkel a ein");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double R = Math.Cos(a);
+
+            double db = z * m * R;
+            Console.WriteLine("Ergebnis für db: " + db + "cm ");
+
+
+>>>>>>> Stashed changes
         }
     }
 }
