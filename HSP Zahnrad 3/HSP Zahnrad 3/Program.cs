00c08a0792d8694e0248956a2fbbc01b537cc4d3
leonhumborg <72141236+leonhumborg@users.noreply.github.com>
2020-10-27 20:19:19 +0100

    Update Program.cs

--- HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
+++ HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
@@ -6,7 +6,9 @@ namespace HSP_Zahnrad_3
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
+// Updated upstream
+            
+            //Parametereingabe
             Console.WriteLine("Geben Sie den Modul ein");
             double m = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Geben Sie die Zähnezahl ein");
@@ -15,30 +17,31 @@ namespace HSP_Zahnrad_3
             double d = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Geben Sie die Breite ein");
             double b = Convert.ToDouble(Console.ReadLine());
-=======
-            
-
-
-            Console.WriteLine("Geben Sie den Modul ein");
-            double m = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Geben Sie den Teilkreisdurchmesser ein");
-            double d = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Geben Sie das Kopfspiel ein");
-            double c = Convert.ToDouble(Console.ReadLine());
-            double df = d - 2 * (m + c);
-            Console.WriteLine("Ergebnis für df: " + df + "mm");
-
-            Console.WriteLine("Gebenn Sie die Zähnezahl ein");
+            Console.WriteLine("Geben Sie die Zähnezahl ein");
             double z = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Bitte geben Sie den Winkel a ein");
             double a = Convert.ToDouble(Console.ReadLine());
+
+
+
+            double p = Math.Pi * m;
+            double c = 0.167 * m;
+            double df = d - 2 * (m + c);
+            double da = m * (z + 2);
+            double hf = m + c;
+            double ha = m;
             double R = Math.Cos(a);
 
             double db = z * m * R;
-            Console.WriteLine("Ergebnis für db: " + db + "cm ");
 
+            //Ausgabe
+            Console.WriteLine("Ergebnis für die Zahnkopfhöhe:" + ha + "cm");
+            Console.WriteLine("Ergebnis für die Zahnfußöhe:" + hf + "cm");
+            Console.WriteLine("Ergebnis für die Teilung:" + p);
+            Console.WriteLine("Ergebnis für den Grundkreisdurchmesser:" + db + "cm ");
+            Console.WriteLine("Ergebnis für den Kopfkreisdurchmesser:" + da + "cm");
 
->>>>>>> Stashed changes
+//Stashed changes
         }
     }
 }
