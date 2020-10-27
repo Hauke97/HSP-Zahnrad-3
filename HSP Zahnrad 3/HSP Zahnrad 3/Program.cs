e8779a278a6d07c59d74c6985e07229f50309f0b
Hauke97 <72126928+Hauke97@users.noreply.github.com>
2020-10-27 17:18:23 +0100

    Create Program.cs

---
+++ HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HSP_Zahnrad_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");//hallo
+        }
+    }
+}
