33e0551e9a9227a47d1a49afb523a8310b87a2ab
a-rondorf <72039786+a-rondorf@users.noreply.github.com>
2020-10-31 11:29:51 +0100

    Update Program.cs

--- HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
+++ HSP Zahnrad 3/HSP Zahnrad 3/Program.cs
@@ -6,39 +6,109 @@ namespace HSP_Zahnrad_3
     {
         static void Main(string[] args)
         {
-            
+
             //Parametereingabe
-            Console.WriteLine("Geben Sie den Modul ein");
-            double m = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Geben Sie die Zähnezahl ein");
-            int z = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Geben Sie den Teilkreisdurchmesser ein");
-            double d = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Geben Sie die Breite ein");
-            double b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Bitte geben Sie den Winkel a ein");
-            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wenn Grundkreisdurchmesser gesucht wird, drücken Sie 1");
+            Console.WriteLine("Wenn Zahnkopfhöhe gesucht wird, drücken Sie 2");
+            Console.WriteLine("Wenn Zahnfußhöhe gesucht wird, drücken Sie 3");
+            Console.WriteLine("Wenn Fußkreisdurchmesser gesucht wird, drücken Sie 4");
+            Console.WriteLine("Wenn Teilung gesucht wird, drücken Sie 5");
+            Console.WriteLine("Wenn Zahnhöhe gesucht wird, drücken Sie 6");
 
 
-            //Berechnungen
-            double p = Math.PI * m;
-            double c = 0.167 * m;
-            double df = d - 2 * (m + c);
-            double da = m * (z + 2);
-            double hf = m + c;
-            double ha = m;
-            double R = Math.Cos(a);
-            double db = z * m * R;
-            double h = 2 * m + c;
+            int Eingabe = 0;
+
+            Eingabe = Convert.ToInt32(Console.ReadLine());
+
+            if (Eingabe == 1)
+
+            {
+                Console.WriteLine("Geben Sie den Modul ein");
+                double m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben Sie die Zähnezahl ein");
+                int z = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Geben Sie den Winkel ein");
+                double a = Convert.ToDouble(Console.ReadLine());
+                double R = Math.Cos(a);
+                double db = z * m * R;
+                Console.WriteLine("Ergebnis für den Grundkreisdurchmesser:" + db + "mm ");
+
+            }
+
+            else if (Eingabe == 2)
+
+            {
+                Console.WriteLine("Geben Sie den Modul ein");
+                double m = Convert.ToDouble(Console.ReadLine());
+                double ha = m;
+                Console.WriteLine("Ergebnis für die Zahnkopfhöhe:" + ha + "mm");
+
+            }
+
+            else if (Eingabe == 3)
+
+            {
+                Console.WriteLine("Geben Sie den Modul ein");
+                double m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Geben Sie das Kofspiel ein ");
+                double c = Convert.ToDouble(Console.ReadLine());
+                double hf = m + c;
+                Console.WriteLine("Ergebnis für die Zahnfußöhe:" + hf + "mm");
+
+
+            }
+
+            else if (Eingabe == 4)
+
+            {
+                Console.WriteLine("Geben Sie den Modul ein");
+                double m = Convert.ToDouble(Console.ReadLine());
+                double c = 0.167 * m;
+                Console.WriteLine("Geben Sie den Teilkreisdurchmesser ein");
+                double d = Convert.ToDouble(Console.ReadLine());                            
+                double df = d - 2 * (m + c);
+                Console.WriteLine("Ergebnis für den Fußkreisdurchmesser:" + df + "mm");
+            }
+            else if (Eingabe == 5)
+            {
+                Console.WriteLine("Geben Sie den Modul ein");
+                double m = Convert.ToDouble(Console.ReadLine());
+                double p = Math.PI * m;
+                Console.WriteLine("Ergebnis für die Teilung:" + p);
+
+            }
+
+            else if (Eingabe == 6)
+
+            {
+                Console.WriteLine("Geben Sie den Modul ein");
+                double m = Convert.ToDouble(Console.ReadLine());               
+                double c = 0.167 * m;
+                double h = 2 * m + c;
+                Console.WriteLine("Ergebnis für die Zahnhöhe:" + h + "mm");
+
+
+            }
+          
+
+
+
+           
+
+
             
-            //Ausgabe
-            Console.WriteLine("Ergebnis für die Zahnhöhe:" + h + "mm");
-            Console.WriteLine("Ergebnis für die Zahnkopfhöhe:" + ha + "mm");
-            Console.WriteLine("Ergebnis für die Zahnfußöhe:" + hf + "mm");
-            Console.WriteLine("Ergebnis für die Teilung:" + p);
-            Console.WriteLine("Ergebnis für den Grundkreisdurchmesser:" + db + "mm ");
-            Console.WriteLine("Ergebnis für den Kopfkreisdurchmesser:" + da + "mm");
-            Console.WriteLine("Ergebnis für den Fußkreisdurchmesser:" + df+"mm");
+           
+            
+           
+            
+           
+           
+           
+          
+           
+          
+            
+            
 
         }
     }
